af0b53aea7e12c85aeb6ca7ded7c9ea9320deb1d
Michael Wybraniec <michaelwybraniec@me.com>
2022-03-15 00:23:32 +0100

    Styling Vue Components

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,12 +1,38 @@
 <template>
-    <div>Hello from Page Home</div>
-    <div v-for="thread in threads" :key="thread.id">
-      <h2>{{thread.title}}</h2>
-      <div v-for="postId in thread.posts" :key="postId">
-        <p>{{userById(postById(postId).userId).name}}</p>
-        <p>{{posts.find(p => p.id === postId).text}}</p>
+  <div v-for="thread in threads" :key="thread.id" class="col-large push-top">
+    <h1>{{ thread.title }}</h1>
+    <div class="post-list">
+      <div class="post" v-for="postId in thread.posts" :key="postId">
+        <div class="user-info">
+          <a href="#" class="user-name">{{
+            userById(postById(postId).userId).name
+          }}</a>
+
+          <a href="#">
+            <img
+              class="avatar-large"
+              :src="userById(postById(postId).userId).avatar"
+              alt=""
+            />
+          </a>
+
+          <p class="desktop-only text-small">107 posts</p>
+        </div>
+
+        <div class="post-content">
+          <div>
+            <p>
+              {{ postById(postId).text }}
+            </p>
+          </div>
+        </div>
+
+        <div class="post-date text-faded">
+          {{ postById(postId).publishedAt }}
+        </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -21,14 +47,13 @@ export default {
   },
   methods: {
     postById (postId) {
-      return this.posts.find(p => p.id === postId)
+      return this.posts.find((p) => p.id === postId)
     },
     userById (userId) {
-      return this.users.find(u => u.id === userId)
+      return this.users.find((u) => u.id === userId)
     }
   }
 }
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
