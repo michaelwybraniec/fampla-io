5f74e6653ce31f9aae0a608c752b0d6e4e17ba4b
Michael Wybraniec <michaelwybraniec@me.com>
2022-03-14 23:27:04 +0100

    Import Data From JSON Files in a Vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,14 +27,29 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <div>{{threads}}</div>
   </div>
 </template>
 
 <script>
+
+import sourceData from '@/data.json'
+
+const log = (title = undefined, object = undefined) => {
+  return console.log(`HelloWorlds.vue: ${title}`, object)
+}
+
+log('sourceData: ', sourceData)
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data () {
+    return {
+      threads: sourceData.threads
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,4 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 const forumApp = createApp(App)
-// forumApp.component('NiceButon', {})
-// forumApp.use(somePlugin)
 forumApp.mount('#app')
-
-// const anotherVueApp = createApp(App)
-// anotherVueApp.use(AnotherPlugin)
-// anotherVueApp.mount('#another-app')
