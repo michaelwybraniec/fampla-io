09eae631051c89fd3982f6b95318a105726cbc14
Michael Wybraniec <michaelwybraniec@me.com>
2022-03-19 17:57:31 +0100

    NotFound 404 - A sad cat

--- src/components/PageNotFound.vue
+++ src/components/PageNotFound.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="col-full text-center">
-    <h1>Not found :-(</h1>
+    <img src="https://emoji.gg/assets/emoji/8268-cat-sad.png">
+    <h1>Not found</h1>
     <router-link :to="{name: 'Home'}">Read some cool threads</router-link>
   </div>
 </template>
