f9a734f847e4110eb1da527db1b78986b3b76203
Michael Wybraniec <michaelwybraniec@me.com>
2022-04-04 23:38:53 +0200

    Firebase Setup and Installation

--- src/main.js
+++ src/main.js
@@ -3,10 +3,19 @@ import App from './App.vue'
 import router from '@/router'
 import store from '@/store'
 
+// firebase
+import firebaseConfig from '@/config/firebase'
+import { getAnalytics, logEvent } from 'firebase/analytics'
+import { initializeApp } from 'firebase/app'
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
+logEvent(analytics, 'notification_received')
+
 const forumApp = createApp(App)
 forumApp.use(router)
 forumApp.use(store)
 
+// importing all components
 // https://v2.vuejs.org/v2/style-guide/?redirect=true#Base-component-names-strongly-recommended
 const requireComponent = require.context('./components', true, /App[A-Z]\w+\.(vue|js)$/)
 requireComponent.keys().forEach(function (fileName) {
