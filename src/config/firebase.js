446e4d7d192abb641b86b471a72979bbf6cced3d
Michael Wybraniec <michaelwybraniec@me.com>
2022-04-12 22:17:08 +0200

    Using .env for Different Environment Configuration

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,11 +1,10 @@
 module.exports = {
   // Web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  apiKey: 'AIzaSyC7JT3zb5RFr91wAiTClO2hFFAmzvMKNbo',
-  authDomain: 'vue-school-forum-a16cb.firebaseapp.com',
-  projectId: 'vue-school-forum-a16cb',
-  storageBucket: 'vue-school-forum-a16cb.appspot.com',
-  messagingSenderId: '713637615796',
-  appId: '1:713637615796:web:a6fa2cc52786dc4ed69fe1',
-  measurementId: 'G-54QM6ERND5'
+  apiKey: 'AIzaSyBEOEoSj1vPmcq_qSPdhQjlTdXYuz6afkM',
+  authDomain: 'vue-school-forum-19467.firebaseapp.com',
+  projectId: 'vue-school-forum-19467',
+  storageBucket: 'vue-school-forum-19467.appspot.com',
+  messagingSenderId: '49977500509',
+  appId: '1:49977500509:web:7ce08908132b743fb0beb4'
 }
