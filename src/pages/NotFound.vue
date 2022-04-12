b9732f05a1915d41e983fecfac3170902a97e58a
Michael Wybraniec <michaelwybraniec@me.com>
2022-04-12 19:54:36 +0200

    Displaying a Loading Indicator while Components Fetch Async Data

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="col-full text-center">
-    <img src="https://emoji.gg/assets/emoji/8268-cat-sad.png">
+    <img src="https://emoji.gg/assets/emoji/8268-cat-sad.png" />
     <h1>Not found</h1>
-    <router-link :to="{name: 'Home'}">Read some cool threads</router-link>
+    <router-link :to="{ name: 'Home' }">Read some cool threads</router-link>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  created() {
+    this.$emit('ready')
+  }
+}
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
