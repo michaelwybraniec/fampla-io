2092a44505017c134e7ee08f05110de7b3fbc46c
Michael Wybraniec <michaelwybraniec@me.com>
2022-04-20 23:48:42 +0200

    Improving a Mobile Navbar
    Closing the Mobile Navbar on Scroll
    
    Closing the Mobile Navbar on Scroll

--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -1,10 +1,15 @@
 <template>
-  <header class="header" id="header">
+  <header
+    class="header"
+    id="header"
+    v-click-outside="() => (mobileNavMenu = false)"
+    v-page-scroll="() => (mobileNavMenu = false)"
+  >
     <router-link :to="{ name: 'Home' }" class="logo">
       <img src="../assets/svg/house.svg" />
     </router-link>
 
-    <div class="btn-hamburger">
+    <div class="btn-hamburger" @click="mobileNavMenu = !mobileNavMenu">
       <!-- use .btn-humburger-active to open the menu -->
       <div class="top bar"></div>
       <div class="middle bar"></div>
@@ -12,7 +17,7 @@
     </div>
 
     <!-- use .navbar-open to open nav -->
-    <nav class="navbar">
+    <nav class="navbar" :class="{ 'navbar-open': mobileNavMenu }">
       <ul>
         <li v-if="authUser" class="navbar-user">
           <a
@@ -36,9 +41,7 @@
 
           <!-- dropdown menu -->
           <!-- add class "active-drop" to show the dropdown -->
-          <!-- <div id="user-dropdown" :class="{ 'active-drop': userDropdownOpen }"> -->
           <div id="user-dropdown" :class="{ 'active-drop': userDropdownOpen }">
-
             <div class="triangle-drop"></div>
             <ul class="dropdown-menu">
               <li class="dropdown-menu-item">
@@ -47,7 +50,14 @@
                 </router-link>
               </li>
               <li class="dropdown-menu-item">
-                <a @click.prevent="$store.dispatch('auth/signOut')">Sign Out</a>
+                <a
+                  @click.prevent="
+                    $store.dispatch('auth/signOut'),
+                      $router.push({ name: 'Home' })
+                  "
+                >
+                  Sign Out
+                </a>
               </li>
             </ul>
           </div>
@@ -57,6 +67,18 @@
         </li>
         <li v-if="!authUser" class="navbar-item">
           <router-link :to="{ name: 'Register' }">Register</router-link>
+        </li>
+        <li v-if="authUser" class="navbar-mobile-item">
+          <router-link :to="{ name: 'Profile' }">View Profile</router-link>
+        </li>
+        <li v-if="authUser" class="navbar-mobile-item">
+          <a
+            @click.prevent="
+              $store.dispatch('auth/signOut'), $router.push({ name: 'Home' })
+            "
+          >
+            Sign Out
+          </a>
         </li>
       </ul>
 
@@ -91,11 +113,17 @@ export default {
   name: 'TheNavBar',
   data() {
     return {
-      userDropdownOpen: false
+      userDropdownOpen: false,
+      mobileNavMenu: false
     }
   },
   computed: {
     ...mapGetters('auth', ['authUser'])
+  },
+  created() {
+    this.$router.beforeEach((to, from) => {
+      this.mobileNavMenu = false
+    })
   }
 }
 </script>
